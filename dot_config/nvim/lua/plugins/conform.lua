e7cf8b0257b039dc43be8e150f693fbb16141295
Viton Vitanis <viton@rivero.tech>
2025-08-22 23:16:16 +0200

    Extend nvim configuration. Minor modifications to .zshrc

---
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,18 @@
+return {
+    "stevearc/conform.nvim",
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            -- Conform will run multiple formatters sequentially
+            python = { "ruff" },
+            -- You can customize some of the format options for the filetype (:help conform.format)
+            rust = { "rustfmt" },
+            -- Conform will run the first available formatter
+        },
+        format_on_save = {
+            -- These options will be passed to conform.format()
+            timeout_ms = 500,
+            lsp_format = "fallback",
+        },
+    },
+}
